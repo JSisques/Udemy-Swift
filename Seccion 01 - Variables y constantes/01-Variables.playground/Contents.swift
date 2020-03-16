60bbca8ded1c1e193cb2f4a6669f114731e0c111
Javi <javierplazasisques@gmail.com>
2020-03-16 16:02:17 +0100

    16/03/2020

---
+++ Seccion 01 - Variables y constantes/01-Variables.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+//Esto es una constante
+let maximunNumberOfLogins = 10
+
+//Esto es una variable
+var currentLoginAttemp = 0
+
+//Se pueden definir varias variables en una misma linea de la siguiente forma:
+var x = 0, y = 0, z = 0
+
+//Podemos definir el tipo de la variable de la siguiente forma:
+var welcomeMessage: String
+welcomeMessage = "Hola"
+
+
+
+//Podemos definir el mismo tipo para varias variables de la siguiente forma:
+var red, green, blue: Double
+
+//Se puede tener cualquier caracter UNICODE para nombrar variables
+var π = 3.14159265
+var 🐶 = "Perro"
+
+//Para imprimir podemos usar la funcion print()
+print(π)
+print(🐶)
+
+//Si queremos concatenar algo al final de una frase podemos hacerlo de la siguiente forma:
+print("Me acaban de saludadar diciendo: \(welcomeMessage)")
+
+/*
+ Esto es un comentario
+ multilinea
+ */
---
+++ Seccion 01 - Variables y constantes/02-Numeros.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+//NUMEROS ENTEROS
+
+//El valor de un UInt8 es 0
+let minValue = UInt8.min
+//El valor de un UInt8 es 255
+let maxValue = UInt8.max
+
+//Declaracion de un numero entero
+var miEntero: Int = 6
+
+//Valores de 32 bits
+print("Valores de 32 bits: (\(Int32.min), \(Int32.max))")
+//Valores de 64 bits
+print("Valores de 64 bits: (\(Int64.min), \(Int64.max))")
+
+//NUMEROS DECIMALES
+let π = 3.14159265
+let minT = -273.15
+
+//LITERALES NUMERICOS
+
+//Decimal
+var decimalNumber = 17
+
+//Binario
+var binaryNumber = 0b10001
+
+//Octal
+var octalNumber = 0o21 // 17 en octal
+
+//Hexadecimal
+var hexNumber = 0x11 // 17 en hexadecimal
